eb6caa3f085bb1c47cb6e6b9e4ed1e8500846c7f
dep <1649743146@qq.com>
2023-10-30 16:12:00 +0800

    feat: 新增button组件样式

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,10 +1,52 @@
 <template>
-  <button> 按钮 </button>
+  <button :class="classes">
+    <i v-if="loading" class="d-icon-jiazai"></i>
+    <i v-if="icon && !loading" :class="icon"></i>
+    <!-- 如果没有传值就不显示span这个标签 -->
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, PropType } from "vue";
+type buttonType = "primary"|"success"|"danger"|'info'|"text"|"warning"
 export default defineComponent({
   name: "DButton",
+  props: {
+    type: {
+      type: String as PropType<buttonType>,
+      vaildator: (val: string) => {
+        return [ "primary","success","danger",'info',"text","warning"].includes(val) 
+      },
+      default: "primary"
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    plain: Boolean,
+    round: Boolean,
+    circle: Boolean,
+    loading: Boolean,
+    disabled: Boolean,
+  },
+  setup(props, ctx) {
+      const classes = computed(() => [
+        "d-button",
+        //修饰动态的样式
+        "d-button--" + props.type,
+        {
+          "is-plain": props.plain,
+          "is-round": props.round,
+          "is-circle": props.circle,
+          "is-loading": props.loading,
+          "is-disabled": props.disabled
+        }
+      ])
+
+      return {
+        classes
+      }
+  },
 });
 </script>
--- play/App.vue
+++ play/App.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>app
-      <d-button></d-button>
-      <d-Icon name="tabqiehuan"></d-Icon>
-      <d-Icon name="chakan"></d-Icon>
+  <div>
+    <div>
+      <d-button>Default</d-button>
+      <d-button type="primary" loading>Primary</d-button>
+      <d-button type="success">Success</d-button>
+      <d-button type="info">Info</d-button>
+      <d-button type="warning" disabled>Warning</d-button>
+      <d-button type="danger" round>Danger</d-button>
+    </div>
+    <div style="margin: 10px 0;">
+      <d-button plain>Plain</d-button>
+      <d-button type="primary" plain>Primary</d-button>
+      <d-button type="primary" icon="d-icon-tabqiehuan" circle></d-button>
+      <d-button type="success" icon="d-icon-chakan">按钮</d-button>
+      <d-button type="warning" :loading="buttonLoading">加载</d-button>
+    </div>
   </div>
 </template>
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue'
+function useButton() {
+  let buttonLoading = ref(true)
+  onMounted(()=>{
+      setTimeout(() => {
+          buttonLoading.value = false;
+      }, 2000);
+  });
+  return {
+    buttonLoading
+  }
+}
+
+export default defineComponent({
+  setup() {
+    return {
+      ...useButton()
+    }
+  },
+})
+
+</script>
